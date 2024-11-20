26108e018b6b5c856fa99b203a78b6c43e750602
Joohyun Kim <corinthionia@gmail.com>
2024-11-21 02:01:40 +0900

    chore(eslint-config): upgrade eslint plugins to latest versions

--- packages/eslint-config/next.js
+++ packages/eslint-config/next.js
@@ -1,14 +1,16 @@
-const { resolve } = require("node:path");
+const { resolve } = require('node:path');
 
-const project = resolve(process.cwd(), "tsconfig.json");
+const project = resolve(process.cwd(), 'tsconfig.json');
 
 /** @type {import("eslint").Linter.Config} */
 module.exports = {
   extends: [
-    "eslint:recommended",
-    "prettier",
-    require.resolve("@vercel/style-guide/eslint/next"),
-    "turbo",
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'prettier',
+    'turbo',
+    'next',
   ],
   globals: {
     React: true,
@@ -18,18 +20,14 @@ module.exports = {
     node: true,
     browser: true,
   },
-  plugins: ["only-warn"],
+  plugins: ['only-warn'],
   settings: {
-    "import/resolver": {
+    'import/resolver': {
       typescript: {
         project,
       },
     },
   },
-  ignorePatterns: [
-    // Ignore dotfiles
-    ".*.js",
-    "node_modules/",
-  ],
-  overrides: [{ files: ["*.js?(x)", "*.ts?(x)"] }],
+  ignorePatterns: ['.*.js', 'node_modules/'],
+  overrides: [{ files: ['*.js?(x)', '*.ts?(x)'] }],
 };
