f5eb363fa6a1446f8df7f539c61017fcde425724
kyoul10121 <kyoul10121@naver.com>
2024-11-22 14:36:29 +0900

    design(apps): 카카오 로그인 버튼 구현

--- apps/daengle/pages/index.tsx
+++ apps/daengle/pages/index.tsx
@@ -1,3 +1,9 @@
+import Login from '~/pages/login';
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div>
+      <Login />
+    </div>
+  );
 }
---
+++ apps/daengle/src/pages/login/index.tsx
@@ -0,0 +1,11 @@
+import styles from './index.module.scss';
+
+export default function Login(): JSX.Element {
+  return (
+    <div className={styles.wrapper}>
+      <button className={styles.buttonBox}>
+        <span className={styles.buttonTextBox}>카카오톡으로 시작하기</span>
+      </button>
+    </div>
+  );
+}
