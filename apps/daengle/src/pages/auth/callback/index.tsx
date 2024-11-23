1da48308e3fab93665bf4e632833637b3259222e
kyoul10121 <kyoul10121@naver.com>
2024-11-24 03:36:08 +0900

    refactor(daengle): 코드리뷰를 바탕으로 리팩토링

--- apps/daengle/src/pages/auth/callback/index.tsx
+++ apps/daengle/src/pages/auth/callback/index.tsx
@@ -1,21 +1,22 @@
 import { useEffect } from 'react';
-import styles from './index.module.scss';
 import axios from 'axios';
 import { useRouter } from 'next/router';
 
-export default function Redirection() {
-  const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
-  const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI;
+const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
+const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI;
+const KAKAO_OAUTH_TOKEN_URL = process.env.NEXT_PUBLIC_KAKAO_OAUTH_TOKEN_URL;
+const KAKAO_OAUTH_BACKEND_URL = process.env.NEXT_PUBLIC_KAKAO_OAUTH_BACKEND_URL;
+
+export default function Callback() {
   const router = useRouter();
   const { code } = router.query || {};
-  console.log('Authorization Code:', code);
 
   useEffect(() => {
     const fetchAccessToken = async () => {
       if (!code) return;
 
       try {
-        const response = await axios.post('https://kauth.kakao.com/oauth/token', null, {
+        const response = await axios.post(KAKAO_OAUTH_TOKEN_URL, null, {
           params: {
             grant_type: 'authorization_code',
             client_id: REST_API_KEY,
@@ -27,16 +28,13 @@ export default function Redirection() {
           },
         });
         const { access_token } = response.data;
-        console.log('access_token=>>', access_token);
 
         // 액세스 토큰 백엔드한테 보내주기
-        const serverResponse = await axios.post('https://dev-api.daengle.com/api/oauth/kakao', {
+        const serverResponse = await axios.post(KAKAO_OAUTH_BACKEND_URL, {
           kakaoAccessToken: access_token,
           loginType: 'GROOMER',
         });
-        console.log('serverResponse 나와라 ! ', serverResponse);
       } catch (error) {
-        console.error('카카오 로그인 실패:', error);
         alert('로그인 실패. 다시 시도해주세요.');
       }
     };
@@ -45,7 +43,7 @@ export default function Redirection() {
   }, [code]);
 
   return (
-    <div className={styles.wrapper}>
+    <div>
       <p>Loading...</p>
     </div>
   );
--- apps/daengle/src/pages/auth/index.tsx
+++ apps/daengle/src/pages/auth/index.tsx
@@ -1,10 +1,10 @@
 import styles from './index.module.scss';
 
-export default function Login() {
-  const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
-  const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI;
-  const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
+const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
+const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI;
+const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
 
+export default function Auth() {
   const handleKakaoLoginClick = () => {
     window.location.href = kakaoAuthUrl;
   };
