09bfc05e9a1a804ccb5ce99791561204e8183ace
kyoul10121 <kyoul10121@naver.com>
2024-11-24 22:25:43 +0900

    refactor(daengle): 컨벤션에 맞게 변수명과 폴더명 수정

--- apps/daengle/src/api/axios.ts
+++
@@ -1,7 +0,0 @@
-import axios from 'axios';
-export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-
-export const instance = axios.create({
-  baseURL: BASE_URL,
-  headers: { 'Content-Type': 'application/json' },
-});
