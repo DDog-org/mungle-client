2b10bff776879547c10c586bdb9265228e49a20a
Joohyun Kim <corinthionia@gmail.com>
2024-11-25 02:49:39 +0900

    docs(storybook): update text stories docs

--- apps/storybook/stories/text/index.stories.tsx
+++ apps/storybook/stories/text/index.stories.tsx
@@ -9,10 +9,15 @@ const meta = {
     tag: {
       control: 'select',
       options: Object.keys(TAGS),
+      description: 'HTML 텍스트 태그를 지정합니다. 기본값은 `span` 입니다.',
+    },
+    children: {
+      description: '텍스트 내용을 전달합니다.',
     },
     typo: {
       control: 'select',
       options: Object.keys(theme.typo),
+      description: '텍스트 스타일을 지정합니다.',
     },
   },
 } satisfies Meta<typeof Text>;
