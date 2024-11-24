0c8949a461c87184b31a80b6e90a2e8e20a803db
kyoul10121 <kyoul10121@naver.com>
2024-11-24 22:08:27 +0900

    refactor(daengle): api 호출 부분 api/auth/index.tsx에 분리

---
+++ apps/daengle/src/api/axios.ts
@@ -0,0 +1,7 @@
+import axios from 'axios';
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+export const instance = axios.create({
+  baseURL: BASE_URL,
+  headers: { 'Content-Type': 'application/json' },
+});
--- apps/daengle/src/pages/auth/callback/index.tsx
+++ apps/daengle/src/pages/auth/callback/index.tsx
@@ -1,45 +1,26 @@
 import { useEffect } from 'react';
-import axios from 'axios';
 import { useRouter } from 'next/router';
-
-const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
-const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI;
-const KAKAO_OAUTH_TOKEN_URL = process.env.NEXT_PUBLIC_KAKAO_OAUTH_TOKEN_URL;
-const KAKAO_OAUTH_BACKEND_URL = process.env.NEXT_PUBLIC_KAKAO_OAUTH_BACKEND_URL;
+import { fetchKakaoAccessToken, sendAccessTokenToBackend } from '~/api/auth';
 
 export default function Callback() {
   const router = useRouter();
   const { code } = router.query || {};
 
   useEffect(() => {
-    const fetchAccessToken = async () => {
+    const handleAuthentication = async () => {
       if (!code) return;
 
       try {
-        const response = await axios.post(KAKAO_OAUTH_TOKEN_URL, null, {
-          params: {
-            grant_type: 'authorization_code',
-            client_id: REST_API_KEY,
-            redirect_uri: REDIRECT_URI,
-            code: code,
-          },
-          headers: {
-            'Content-Type': 'application/x-www-form-urlencoded',
-          },
-        });
-        const { access_token } = response.data;
+        const accessToken = await fetchKakaoAccessToken(code as string);
 
         // 액세스 토큰 백엔드한테 보내주기
-        const serverResponse = await axios.post(KAKAO_OAUTH_BACKEND_URL, {
-          kakaoAccessToken: access_token,
-          loginType: 'GROOMER',
-        });
+        await sendAccessTokenToBackend(accessToken, 'GROOMER');
       } catch (error) {
         alert('로그인 실패. 다시 시도해주세요.');
       }
     };
 
-    fetchAccessToken();
+    handleAuthentication();
   }, [code]);
 
   return (
