fe47f48c9780e280b93ab818d5abc13b9130f969
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-11-25 02:16:43 +0900

    feat(groomer): 미용사 견적서 상세조회 렌더

---
+++ apps/groomer/src/pages/sheet/index.tsx
@@ -0,0 +1,9 @@
+import { SheetDetail } from '@daengle/services';
+
+export default function Sheet() {
+  return (
+    <div>
+      <SheetDetail />
+    </div>
+  );
+}
--- packages/services/src/index.ts
+++ packages/services/src/index.ts
@@ -1,1 +1,2 @@
 export { default as SheetList } from './sheet/components/sheet-list/index';
+export { default as SheetDetail } from './sheet/components/sheet/index';
