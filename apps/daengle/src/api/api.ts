e6bd175ea159582bdbc0933efc8d74efb99c49c6
김윤일 <98331998+kyoul10121@users.noreply.github.com>
2024-11-26 10:03:32 +0900

    [Feature/package] 카카오 로그인 api 연동 (#49)
    * feat(daengle): 카카오 로그인 인가코드 받기
    
    * feat(daengle): 카카오 로그인 토큰 받기
    
    * feat(daengle): 카카오 로그인 토큰 서버로 보내기
    
    * chore(daengle): 불필요한 파일 삭제
    
    * refactor(daengle): 코드리뷰를 바탕으로 리팩토링
    
    * refactor(daengle): api 호출 부분 api/auth/index.tsx에 분리
    
    * refactor(daengle): 컨벤션에 맞게 변수명과 폴더명 수정
    
    * feat(daengle): react-query 기본 적용
    
    * refactor(daengle): react-query query-keys 컨벤션에 맞게 수정
    
    * refactor(daengle): react-query 적용
    
    * refactor(daengle): 코드리뷰 토대로 리팩토링
    
    * refactor(daengle): 수정한 컨벤션에 맞게 변수명 수정
    
    * refactor(daengle): base url 수정

--- apps/daengle/src/api/api.ts
+++ apps/daengle/src/api/api.ts
@@ -1,18 +1,7 @@
 import axios from 'axios';
-import { useAuthStore } from '~/stores/oauth';
 export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
 export const api = axios.create({
   baseURL: BASE_URL,
   headers: { 'Content-Type': 'application/json' },
-});
-
-// Axios 요청 인터셉터 설정
-api.interceptors.request.use((config) => {
-  const accessToken = useAuthStore.getState().accessToken; // zustand에서 accessToken 가져오기
-
-  if (accessToken) {
-    config.headers.Authorization = `Bearer ${accessToken}`; // 헤더에 토큰 추가
-  }
-  return config;
 });
