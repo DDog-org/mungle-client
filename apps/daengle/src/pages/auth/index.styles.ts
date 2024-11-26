29fc055f03552f377fa332eaff74c5072c2c090b
kyoul10121 <kyoul10121@naver.com>
2024-11-26 10:36:04 +0900

    design(daengle): emotion 적용

--- apps/daengle/src/pages/auth/index.styles.ts
+++ apps/daengle/src/pages/auth/index.styles.ts
@@ -1,11 +1,13 @@
-.wrapper {
+import { css } from '@emotion/react';
+
+const wrapper = css`
   display: flex;
   justify-content: center;
   align-items: center;
   height: 100vh;
-}
+`;
 
-.buttonBox {
+const buttonBox = css`
   display: flex;
   align-items: center;
   justify-content: center;
@@ -16,8 +18,16 @@
   cursor: pointer;
 
   background-color: #fee500;
-}
-.buttonTextBox {
+`;
+const buttonTextBox = css`
   font-size: 14px;
   font-weight: 500;
-}
+`;
+
+const styles = {
+  wrapper,
+  buttonBox,
+  buttonTextBox,
+};
+
+export default styles;
--- apps/daengle/src/pages/auth/index.tsx
+++ apps/daengle/src/pages/auth/index.tsx
@@ -1,3 +1,5 @@
+import styles from './index.styles';
+
 const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
 const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI;
 const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
@@ -7,9 +9,9 @@ export default function Auth() {
     window.location.href = kakaoAuthUrl;
   };
   return (
-    <div>
-      <button onClick={handleKakaoLoginClick}>
-        <span>카카오톡으로 시작하기</span>
+    <div css={styles.wrapper}>
+      <button css={styles.buttonBox} onClick={handleKakaoLoginClick}>
+        <span css={styles.buttonTextBox}>카카오톡으로 시작하기 zz</span>
       </button>
     </div>
   );
