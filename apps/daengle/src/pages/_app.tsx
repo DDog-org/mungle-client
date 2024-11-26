3474c281a80ca157c758c2123e70ac872c0131f4
김윤일 <98331998+kyoul10121@users.noreply.github.com>
2024-11-26 10:03:32 +0900

    [Feature/package] 카카오 로그인 api 연동 (#49)
    * feat(daengle): 카카오 로그인 인가코드 받기
    
    * feat(daengle): 카카오 로그인 토큰 받기
    
    * feat(daengle): 카카오 로그인 토큰 서버로 보내기
    
    * chore(daengle): 불필요한 파일 삭제
    
    * refactor(daengle): 코드리뷰를 바탕으로 리팩토링
    
    * refactor(daengle): api 호출 부분 api/auth/index.tsx에 분리
    
    * refactor(daengle): 컨벤션에 맞게 변수명과 폴더명 수정
    
    * feat(daengle): react-query 기본 적용
    
    * refactor(daengle): react-query query-keys 컨벤션에 맞게 수정
    
    * refactor(daengle): react-query 적용
    
    * refactor(daengle): 코드리뷰 토대로 리팩토링
    
    * refactor(daengle): 수정한 컨벤션에 맞게 변수명 수정
    
    * refactor(daengle): base url 수정

--- apps/daengle/src/pages/_app.tsx
+++ apps/daengle/src/pages/_app.tsx
@@ -1,15 +1,19 @@
 import type { AppProps } from 'next/app';
 import { GlobalStyle, Layout } from '@daengle/design-system';
 import { initMSW } from '~/mocks/init-msw';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 initMSW();
+const queryClient = new QueryClient();
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <GlobalStyle>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </GlobalStyle>
+    <QueryClientProvider client={queryClient}>
+      <GlobalStyle>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </GlobalStyle>
+    </QueryClientProvider>
   );
 }
---
+++ apps/daengle/src/pages/auth/callback/index.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+import { useFetchKakaoAccessToken, usePostApiOauthKakao } from '~/queries/oauth';
+
+export default function AuthCallback() {
+  const router = useRouter();
+  const { code } = router.query || {};
+
+  const { mutateAsync: postKakaoOauth } = useFetchKakaoAccessToken();
+
+  const { mutateAsync: postOauthKakao } = usePostApiOauthKakao();
+
+  useEffect(() => {
+    const handleAuthentication = async () => {
+      if (!code) return;
+      try {
+        const accessToken = await postKakaoOauth(code as string);
+        // 액세스 토큰 백엔드한테 보내주기
+        await postOauthKakao({ accessToken, loginType: 'GROOMER' });
+
+        router.push('/home'); // 로그인 성공 시 홈으로 이동
+      } catch (error) {
+        throw new Error('로그인 실패');
+      }
+    };
+
+    handleAuthentication();
+  }, [code, postKakaoOauth, postOauthKakao, router]);
+
+  return (
+    <div>
+      <p>Loading...</p>
+    </div>
+  );
+}
