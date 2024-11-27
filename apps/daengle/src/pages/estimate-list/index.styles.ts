e3797da919f5f0822458a5edb7eebc00f0a1a1e0
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-11-27 16:47:33 +0900

    refactor(daengle): design-system 컴포넌트 마이그레이션

---
+++ apps/daengle/src/pages/estimate-list/index.styles.ts
@@ -0,0 +1,7 @@
+import { css } from '@emotion/react';
+
+export const wrapper = css`
+  min-height: 100%;
+  margin-bottom: 100px;
+  background-color: #f3f5f8;
+`;
--- apps/daengle/src/pages/estimate-list/index.tsx
+++ apps/daengle/src/pages/estimate-list/index.tsx
@@ -1,4 +1,71 @@
 import { UserEstimateList } from '@daengle/services';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
+import { GNB } from '@daengle/design-system';
+import { wrapper } from './index.styles';
+import {
+  GnbChattingActive,
+  GnbChattingInactive,
+  GnbHomeActive,
+  GnbHomeInactive,
+  GnbMyActive,
+  GnbMyInactive,
+  GnbReservationActive,
+  GnbReservationInactive,
+  GnbSheetActive,
+  GnbSheetInactive,
+} from '@daengle/design-system/icons';
+
+export const PATHS = {
+  SHEET: '/estimate-list',
+  RESERVATION: '/reservation',
+  HOME: '/',
+  CHATTING: '/chatting',
+  MYPAGE: '/mypage',
+} as const;
+
+export const MENUS = [
+  {
+    name: '견적',
+    icon: {
+      active: <GnbSheetActive width="32px" height="32px" />,
+      inactive: <GnbSheetInactive width="32px" height="32px" />,
+    },
+    path: PATHS.SHEET,
+  },
+  {
+    name: '예약',
+    icon: {
+      active: <GnbReservationActive width="32px" height="32px" />,
+      inactive: <GnbReservationInactive width="32px" height="32px" />,
+    },
+    path: PATHS.RESERVATION,
+  },
+  {
+    name: '홈',
+    icon: {
+      active: <GnbHomeActive width="32px" height="32px" />,
+      inactive: <GnbHomeInactive width="32px" height="32px" />,
+    },
+    path: PATHS.HOME,
+  },
+  {
+    name: '채팅',
+    icon: {
+      active: <GnbChattingActive width="32px" height="32px" />,
+      inactive: <GnbChattingInactive width="32px" height="32px" />,
+    },
+    path: PATHS.CHATTING,
+  },
+  {
+    name: '마이',
+    icon: {
+      active: <GnbMyActive width="32px" height="32px" />,
+      inactive: <GnbMyInactive width="32px" height="32px" />,
+    },
+    path: PATHS.MYPAGE,
+  },
+];
 
 const petInfos = [
   {
@@ -11,16 +78,16 @@ const petInfos = [
         name: '미용사A',
         daengleMeter: 10,
         image: 'https://placehold.co/400',
-        shopName: null,
+        shopName: '꼬꼬마 관리샵',
         reservedDate: '2024-11-25 11:33:22',
-        tags: ['대형견, 노견'],
+        tags: ['대형견', '노견'],
       },
       {
         id: 19,
         name: '미용사A',
         daengleMeter: 20,
         image: 'https://placehold.co/400',
-        shopName: null,
+        shopName: '뽀까샵',
         reservedDate: '2024-11-25 11:33:22',
       },
       {
@@ -28,9 +95,9 @@ const petInfos = [
         name: '미용사A',
         daengleMeter: 30,
         image: 'https://placehold.co/400',
-        shopName: null,
+        shopName: '빠까뽀',
         reservedDate: '2024-11-25 11:33:22',
-        tags: ['대형견, 노견'],
+        tags: ['대형견', '노견'],
       },
     ],
     careEstimates: [
@@ -70,9 +137,18 @@ const petInfos = [
 ];
 
 export default function EstimatePage() {
+  const router = useRouter();
+  const [activePath] = useState(PATHS.SHEET);
+
+  const handleNavigate = (path: string) => {
+    // 임시 경로
+    router.push(path);
+  };
+
   return (
-    <div>
+    <div css={wrapper}>
       <UserEstimateList petInfos={petInfos} />
+      <GNB menus={MENUS} activePath={activePath} onNavigate={handleNavigate} />
     </div>
   );
 }
