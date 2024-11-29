47c632ef3c838bd61a39c3adb2017d9449f8c299
yunil kim <98331998+cabrerainbu@users.noreply.github.com>
2024-11-29 13:53:24 +0900

    [Design/daengle] 사용자 프로필 수정 UI 구현 (#105)
    * design(daengle): 기본 레이아웃 구현
    
    * design(daengle): 기본 레이아웃 구현 및 시멘틱 태그 적용
    
    * design(daengle): 개발된 storybook 적용
    
    * design(daengle): profile 폴더명 edit-profile 로 수정
    
    * chore(daengle): 컴포넌트 수정 및 불필요한 파일 삭제
    
    * design(daengle): wrapper 에 전체 padding 적용
    
    * design(daengle): 수정 불가한 부분 Text 컴포넌트로 수정
    
    * refactor(daengle): 현재 반영된 storybook 적용
    
    * chore(daengle): 불필요한 코드 삭제
    
    * design(daengle): 버튼 컴포넌트에 옵션 추가하여 사이즈 수정
    
    * refactor(daengle): 코드리뷰 바탕으로 리팩토링

---
+++ apps/daengle/src/pages/edit-profile/index.styles.tsx
@@ -0,0 +1,48 @@
+import { colors } from '@daengle/design-system';
+import { css } from '@emotion/react';
+
+export const wrapper = css`
+  padding: 18px;
+`;
+export const container = css`
+  width: 393px;
+  height: 852px;
+  display: flex;
+  flex-direction: column;
+  border: 1px solid red;
+`;
+export const profileImageWrapper = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 32px 0 40px;
+`;
+export const profileEditButtonBox = css`
+  margin-top: 12px;
+  font-size: 14px;
+  color: ${colors.gray400};
+`;
+export const inputWrapper = css`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+  margin: 0 0 159px;
+`;
+export const nickNameWrapper = css`
+  display: flex;
+  align-items: flex-end;
+`;
+export const duplicateButtonBox = css`
+  margin: 0 0 18px 4px;
+  width: 59px;
+  height: 30px;
+  border: 1px solid black;
+  border-radius: 15px;
+  font-size: 10px;
+`;
+export const readOnlyTextBox = css`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
