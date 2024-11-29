2e26efdcc8b187d25cd2e479bdac7a8a6ea0443e
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-11-30 02:30:14 +0900

    design(daengle): 사용자 견적서 상세조회 UI 구현

--- apps/daengle/next.config.mjs
+++ apps/daengle/next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@daengle/design-system'],
+  transpilePackages: ['@daengle/design-system', '@daengle/services'],
 };
 
 export default nextConfig;
