9a563cab44a55e2bcf6ee26f894410ccac1bc4e7
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-12-01 03:29:11 +0900

    design(daengle): 사용자 견적서 리스트 UI 구현
    * feat(services): 사용자 견적서 리스트 UI 구현
    
    * feat(daengle): 사용자 견적서 리스트 렌더
    
    * refactor(services): design-system 컴포넌트 마이그레이션
    
    * refactor(daengle): design-system 컴포넌트 마이그레이션
    
    * design(daengle): 디자인 시스템 마이그레이션 및 AppBar 수정
    
    * design(services): 데이터 없을 시의 UI 구현
    
    * feat(design-system): EmptyStateBone svg 추가
    
    * refactor(daengle): 데이터 없을 시, 테스트
    
    * feat(services): 공란 스타일 추가
    
    * refactor(services): 사용자 견적서 리스트 UI 컴포넌트 분리
    
    * refactor(daengle): 컴포넌트 조합 및 _app.tsx props 삭제

---
+++ packages/services/src/estimate/components/estimate-list/daengle/empty-state/index.styles.ts
@@ -0,0 +1,25 @@
+import { css } from '@emotion/react';
+import { theme } from '@daengle/design-system';
+
+export const emptyStateWrapper = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 50vh;
+  text-align: center;
+  gap: 15px;
+`;
+
+export const emptyButton = css`
+  margin-top: 20px;
+  border-radius: 29.5px;
+  cursor: pointer;
+  text-align: center;
+  ${theme.typo.body4};
+`;
+
+export const emptyText = css`
+  ${theme.typo.subtitle3};
+  color: ${theme.colors.gray400};
+`;
