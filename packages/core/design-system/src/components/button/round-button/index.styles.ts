ff6501663aaad28e2f61ea7f38449cf740c81e8c
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-11-30 10:34:56 +0900

    refactor(design-system): round-button variant 추가

--- packages/core/design-system/src/components/button/round-button/index.styles.ts
+++ packages/core/design-system/src/components/button/round-button/index.styles.ts
@@ -95,6 +95,11 @@ export const buttonVariant = ({
       : theme.colors.greenGradient100};
     color: ${theme.colors.white};
   `}
+  ${variant === 'green' &&
+  css`
+    color: ${theme.colors.white};
+    background: ${theme.colors.greenGradient100};
+  `}
   ${variant === 'ghost' &&
   css`
     border: 1px solid ${theme.colors.gray200};
