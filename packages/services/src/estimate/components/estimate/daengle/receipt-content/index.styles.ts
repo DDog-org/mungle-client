789d56657a909616c03086cceeb9d8c7d4cb1660
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-12-01 22:21:55 +0900

    design(daengle): 사용자 견적서 상세 조회 UI 구현
    * refactor(design-system): app-bar 배경색 선택 가능하도록 수정
    
    * design(services): 사용자 견적서 상세조회 UI 컴포넌트 구현
    
    * design(daengle): 사용자 견적서 상세조회 UI 구현
    
    * design(services): 사용자 견적서 상세 컴포넌트 분리
    
    * design(daengle): Layout 적용 및 컴포넌트 조립

---
+++ packages/services/src/estimate/components/estimate/daengle/receipt-content/index.styles.ts
@@ -0,0 +1,10 @@
+import { css } from '@emotion/react';
+
+export const content = css`
+  display: flex;
+  flex-direction: column;
+  padding: 18px;
+  background-color: #ffffff;
+  gap: 8px;
+  margin: -2px 14px;
+`;
