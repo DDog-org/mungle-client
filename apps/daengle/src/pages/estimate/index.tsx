a155531015d41ee2e273ba335076f11e3ffb25f3
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-12-01 19:14:24 +0900

    design(daengle): Layout 적용 및 컴포넌트 조립

--- apps/daengle/src/pages/estimate/index.tsx
+++ apps/daengle/src/pages/estimate/index.tsx
@@ -1,4 +1,4 @@
-import { AppBar, RoundButton, Text } from '@daengle/design-system';
+import { AppBar, Layout, RoundButton, Text } from '@daengle/design-system';
 import { wrapper, header, section, buttonGroup } from './index.styles';
 import { DesignerInfo, Receipt } from '@daengle/services';
 
@@ -17,37 +17,46 @@ export default function Detail() {
   };
 
   return (
-    <div css={wrapper}>
-      <AppBar isDefaultBackground={false} />
-      <header css={header}>
-        <Text typo="title1">견적 상세</Text>
-      </header>
-      <DesignerInfo profile={designerData} />
-      <section css={section}>
-        <Text typo="body1">소개</Text>
-        <Text typo="body10" tag="div">
-          모든 견종의 가위컷에 자신이 있으며, 특히, 푸들 테디베어컷, 비숑 귀툭튀컷, 포메 곰돌이컷 등
-          디자인컷 완성도에 자신이 있습니다.
-        </Text>
-      </section>
-      <Receipt receipt={designerData} />
-      <div css={buttonGroup}>
-        <RoundButton variant="primary" size="S" fullWidth>
-          <Text typo="body1" color="white">
-            채팅 문의
+    <Layout>
+      <div css={wrapper}>
+        <AppBar isDefaultBackground={false} />
+        <div css={header}>
+          <Text typo="title1">견적 상세</Text>
+        </div>
+        <DesignerInfo profile={designerData} />
+        <section css={section}>
+          <Text typo="body1">소개</Text>
+          <Text typo="body10" tag="div">
+            모든 견종의 가위컷에 자신이 있으며, 특히, 푸들 테디베어컷, 비숑 귀툭튀컷, 포메 곰돌이컷
+            등 디자인컷 완성도에 자신이 있습니다.
           </Text>
-        </RoundButton>
-        <RoundButton
-          variant="primary"
-          size="S"
-          fullWidth
-          onClick={() => alert('예약금 결제를 진행합니다.')}
-        >
-          <Text typo="body1" color="white">
-            예약금 결제
-          </Text>
-        </RoundButton>
+        </section>
+        <Receipt
+          items={[
+            { title: '지역', receipt: designerData.address },
+            { title: '일정', receipt: designerData.reservedDate },
+            { title: '서비스', receipt: designerData.desiredStyle },
+            { title: '추가 소견', receipt: designerData.overallOpinion, typo: 'body4' },
+          ]}
+        />
+        <div css={buttonGroup}>
+          <RoundButton variant="primary" size="S" fullWidth>
+            <Text typo="body1" color="white">
+              채팅 문의
+            </Text>
+          </RoundButton>
+          <RoundButton
+            variant="primary"
+            size="S"
+            fullWidth
+            onClick={() => alert('예약금 결제를 진행합니다.')}
+          >
+            <Text typo="body1" color="white">
+              예약금 결제
+            </Text>
+          </RoundButton>
+        </div>
       </div>
-    </div>
+    </Layout>
   );
 }
