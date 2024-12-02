bbdaf7f6c627329eb163360d2464e9830c8c06d4
Yoonjeong Ko <jejukyj@naver.com>
2024-12-02 15:07:57 +0900

    refactor(daengle): selectBox 컴포넌트 분리 및 각 타이틀 h 태그로 변경

--- apps/daengle/src/pages/estimate-grooming/index.tsx
+++ apps/daengle/src/pages/estimate-grooming/index.tsx
@@ -7,7 +7,6 @@ import {
   registerPet,
   circle,
   selectBox,
-  selectItem,
   textField,
 } from './index.styles';
 import Image from 'next/image';
@@ -17,6 +16,7 @@ import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider, DatePicker, TimePicker } from '@mui/x-date-pickers';
 import dayjs, { Dayjs } from 'dayjs';
 import 'dayjs/locale/ko';
+import EstimateSelectComponent from '~/components/estimate';
 
 export default function EstimateCreate() {
   const router = useRouter();
@@ -34,11 +34,11 @@ export default function EstimateCreate() {
     <Layout>
       <AppBar />
       <div css={wrapper}>
-        <Text typo="title1" color="black">
+        <Text tag="h1" typo="title1" color="black">
           견적요청서
         </Text>
         <section css={section}>
-          <Text typo="subtitle3" color="black">
+          <Text tag="h2" typo="subtitle3" color="black">
             지역
           </Text>
           <Text typo="title2" color="black">
@@ -46,7 +46,7 @@ export default function EstimateCreate() {
           </Text>
         </section>
         <section css={section}>
-          <Text typo="subtitle3" color="black">
+          <Text tag="h2" typo="subtitle3" color="black">
             시술 희망 날짜 및 시간
           </Text>
           <div css={box}>
@@ -95,7 +95,7 @@ export default function EstimateCreate() {
           </div>
         </section>
         <section css={section}>
-          <Text typo="subtitle3" color="black">
+          <Text tag="h2" typo="subtitle3" color="black">
             어떤 아이를 가꿀 예정이신가요?
           </Text>
           <div css={registerPet}>
@@ -118,60 +118,26 @@ export default function EstimateCreate() {
           </div>
         </section>
         <section css={section}>
-          <Text typo="subtitle3" color="black">
+          <Text tag="h2" typo="subtitle3" color="black">
             원하는 미용
           </Text>
           <div css={selectBox}>
-            <div css={selectItem}>
-              <Text typo="body5" color="gray400">
-                전체 클리핑
-              </Text>
-              <Image
-                src="/images/grooming_full_clipping.svg"
-                alt="전체 클리핑"
-                width={129}
-                height={107}
-              />
-            </div>
-            <div css={selectItem}>
-              <Text typo="body5" color="gray400">
-                전체 클리핑 + 얼굴 컷
-              </Text>
-              <Image
-                src="/images/grooming_face_cut.svg"
-                alt="전체 클리핑 + 얼굴 컷"
-                width={129}
-                height={107}
-              />
-            </div>
+            <EstimateSelectComponent name="전체 클리핑" src="/images/grooming_full_clipping.svg" />
+            <EstimateSelectComponent
+              name="전체 클리핑 + 얼굴 컷"
+              src="/images/grooming_face_cut.svg"
+            />
           </div>
           <div css={selectBox}>
-            <div css={selectItem}>
-              <Text typo="body5" color="gray400">
-                전체 가위컷
-              </Text>
-              <Image
-                src="/images/grooming_full_scissor_cut.svg"
-                alt="전체 가위컷"
-                width={129}
-                height={107}
-              />
-            </div>
-            <div css={selectItem}>
-              <Text typo="body5" color="gray400">
-                스포팅 + 얼굴 컷
-              </Text>
-              <Image
-                src="/images/grooming_spotting.svg"
-                alt="스포팅 + 얼굴 컷"
-                width={129}
-                height={107}
-              />
-            </div>
+            <EstimateSelectComponent
+              name="전체 가위컷"
+              src="/images/grooming_full_scissor_cut.svg"
+            />
+            <EstimateSelectComponent name="스포팅 + 얼굴 컷" src="/images/grooming_spotting.svg" />
           </div>
         </section>
         <section css={section}>
-          <Text typo="subtitle3" color="black">
+          <Text tag="h2" typo="subtitle3" color="black">
             추가 요청사항
           </Text>
           <textarea placeholder="추가 요청사항을 입력해주세요" css={textField} />
