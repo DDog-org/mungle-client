47ed2fe67f5c97b91d046dcd8be201146ab24ab6
Yoonjeong Ko <jejukyj@naver.com>
2024-12-03 00:36:12 +0900

    [Design/daengle] 사용자 → 미용사 견적 요청서 폼 UI 구현 #93 (#122)
    * design(daengle): 사용자 견적서 요청폼 UI 구현
    
    * feat(daengle): 날짜 및 시간 선택 기능 구현
    
    * refactor(daengle): 기존 RoundButton CTAButton으로 변경
    
    * refactor(daengle): selectBox 컴포넌트 분리 및 각 타이틀 h 태그로 변경

---
+++ apps/daengle/src/pages/estimate-grooming/index.styles.ts
@@ -0,0 +1,98 @@
+import { theme } from '@daengle/design-system';
+import { css } from '@emotion/react';
+
+export const wrapper = css`
+  width: 100%;
+  padding: 18px 18px 104px 18px;
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`;
+
+export const section = css`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+
+export const box = css`
+  display: flex;
+  gap: 15px;
+`;
+
+export const dateSelect = css`
+  width: 100%;
+  height: 36px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+`;
+
+export const registerPet = css`
+  border: 1px solid ${theme.colors.gray200};
+  border-radius: 10px;
+  width: 100%;
+  height: 109px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 15px;
+`;
+
+export const circle = css`
+  width: 40px;
+  height: 40px;
+  border: 1px solid ${theme.colors.gray200};
+  border-radius: 20px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  cursor: pointer;
+
+  :hover {
+    background-color: ${theme.colors.gray100};
+    transition: 0.3s;
+  }
+`;
+
+export const selectBox = css`
+  display: flex;
+  gap: 13px;
+`;
+
+export const selectItem = css`
+  border: 1px solid ${theme.colors.gray200};
+  border-radius: 10px;
+  width: 100%;
+  height: 195px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 17px;
+  gap: 35px;
+  cursor: pointer;
+
+  :hover {
+    background-color: ${theme.colors.gray100};
+    transition: 0.3s;
+  }
+`;
+
+export const textField = css`
+  width: 100%;
+  height: 135px;
+  background-color: ${theme.colors.gray100};
+  border-radius: 10px;
+  text-align: justify;
+  padding: 18px;
+
+  ::placeholder {
+    color: ${theme.colors.gray300};
+    size: ${theme.typo.body9};
+  }
+`;
