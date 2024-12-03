a13761fe61240795342e9eb7d5f6752435fceb86
yunil kim <98331998+cabrerainbu@users.noreply.github.com>
2024-12-03 16:47:19 +0900

    [Design/daengle] 로그인 UI에 storybook 적용 (#100)
    * chore(apps): 불필요한 타입 선언 삭제
    
    * chore(utils): 기본 프로젝트 설정에 맞게 pnpm-lock.yaml 수정
    
    * design(daengle): emotion 적용
    
    * refactor(daengle): 코드리뷰 바탕으로 리팩토링
    
    * feat(daengle): 화면 크기 적용
    
    * refactor(daengle): storybook 적용

---
+++ apps/daengle/src/pages/auth/index.styles.ts
@@ -0,0 +1,24 @@
+import { css } from '@emotion/react';
+
+export const wrapper = css`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 0 18px 0;
+`;
+export const logoImageBox = css`
+  width: 183px;
+  height: 47px;
+  margin: 0 0 142px;
+`;
+export const buttonBox = css`
+  position: absolute;
+  bottom: 302px;
+`;
+export const buttonTextBox = css`
+  font-size: 14px;
+  font-weight: 500;
+`;
--- apps/daengle/src/pages/auth/index.tsx
+++ apps/daengle/src/pages/auth/index.tsx
@@ -1,3 +1,7 @@
+import { Layout, RoundButton } from '@daengle/design-system';
+import { wrapper, logoImageBox, buttonBox } from './index.styles';
+import Image from 'next/image';
+
 const REST_API_KEY = process.env.NEXT_PUBLIC_REST_API_KEY;
 const REDIRECT_URI = process.env.NEXT_PUBLIC_REDIRECT_URI;
 const kakaoAuthUrl = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}&response_type=code`;
@@ -7,10 +11,15 @@ export default function Auth() {
     window.location.href = kakaoAuthUrl;
   };
   return (
-    <div>
-      <button onClick={handleKakaoLoginClick}>
-        <span>카카오톡으로 시작하기</span>
-      </button>
-    </div>
+    <Layout isAppBarExist={false}>
+      <div css={wrapper}>
+        <div css={logoImageBox}>
+          <Image src="/icons/auth_logo.svg" alt="로고 이미지" width={183} height={47} />
+        </div>
+        <RoundButton variant={'kakao'} size="L" fullWidth onClick={handleKakaoLoginClick}>
+          카카오톡으로 시작하기
+        </RoundButton>
+      </div>
+    </Layout>
   );
 }
