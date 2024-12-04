5f5e6047507eaefddd43f3317fcafa8245ee5648
Joohyun Kim <corinthionia@gmail.com>
2024-12-04 16:56:03 +0900

    fix(groomer): update api endpoint

--- apps/daengle/src/apis/auth/index.ts
+++ apps/daengle/src/apis/auth/index.ts
@@ -10,17 +10,17 @@ import {
 } from '~/models';
 
 export const postJoinWithoutPet = async (body: PostJoinWithoutPetRequestBody) => {
-  return await api.post<PostJoinWithoutPetResponse>('/join-without-pet', body);
+  return await api.post<PostJoinWithoutPetResponse>('/user/join-without-pet', body);
 };
 
 export const postAvailableNickname = async (body: PostAvailableNicknameRequestBody) => {
-  return await api.post<PostAvailableNicknameResponse>('/available-nickname', body);
+  return await api.post<PostAvailableNicknameResponse>('/user/available-nickname', body);
 };
 
 export const getBreedList = async () => {
-  return await api.get<GetBreedListResponse>('/breed-list');
+  return await api.get<GetBreedListResponse>('/user/breed-list');
 };
 
 export const postJoinWithPet = async (body: PostJoinWithPetRequestBody) => {
-  return await api.post<PostJoinWithPetResponse>('/join-with-pet', body);
+  return await api.post<PostJoinWithPetResponse>('/user/join-with-pet', body);
 };
--- apps/groomer/src/apis/auth/index.ts
+++ apps/groomer/src/apis/auth/index.ts
@@ -2,5 +2,5 @@ import { api } from '~/apis';
 import { PostJoinRequestBody, PostJoinResponse } from '~/models/auth';
 
 export const postJoin = async (body: PostJoinRequestBody) => {
-  return await api.post<PostJoinResponse>('/join', body);
+  return await api.post<PostJoinResponse>('/groomer/join', body);
 };
