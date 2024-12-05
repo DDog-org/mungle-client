5c6cfae62b8722119b35599162dcbf5a41ae6373
Joohyun Kim <corinthionia@gmail.com>
2024-12-05 10:44:32 +0900

    task(services): add delete from s3 hook

--- packages/services/src/hooks/use-s3/index.ts
+++ packages/services/src/hooks/use-s3/index.ts
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import { nanoid } from 'nanoid';
 import dotenv from 'dotenv';
-import { S3Client, PutObjectCommand } from '@aws-sdk/client-s3';
+import { S3Client, PutObjectCommand, DeleteObjectCommand } from '@aws-sdk/client-s3';
 
 dotenv.config();
 
@@ -15,7 +15,7 @@ interface Props {
     | 'vet/licenses';
 }
 
-const s3Client = new S3Client({
+const s3 = new S3Client({
   region: process.env.AWS_REGION!,
   credentials: {
     accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
@@ -41,7 +41,7 @@ export function useS3({ targetFolderPath }: Props) {
 
       try {
         const command = new PutObjectCommand(params);
-        await s3Client.send(command);
+        await s3.send(command);
         const url = `https://${process.env.AWS_BUCKET}.s3.${process.env.AWS_REGION}.amazonaws.com/${`${targetFolderPath}/${id}`}`;
         setUploadedURLs((prev) => [...prev, url]);
       } catch (error) {
@@ -52,5 +52,19 @@ export function useS3({ targetFolderPath }: Props) {
     return uploadedURLs;
   };
 
-  return { uploadToS3 };
+  const deleteFromS3 = async (fileName: string) => {
+    try {
+      const deleteParams = {
+        Bucket: process.env.AWS_BUCKET,
+        Key: `${targetFolderPath}/${fileName}`,
+      };
+
+      const command = new DeleteObjectCommand(deleteParams);
+      await s3.send(command);
+    } catch (error) {
+      throw new Error(String(error));
+    }
+  };
+
+  return { uploadToS3, deleteFromS3 };
 }
