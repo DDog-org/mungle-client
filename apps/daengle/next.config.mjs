6f4fa566cc1488fefac754ccf40fea0560a99176
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-12-06 01:09:20 +0900

    refactor(daengle): next.config 재배치 과정 중복 수정

--- apps/daengle/next.config.mjs
+++ apps/daengle/next.config.mjs
@@ -1,14 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  transpilePackages: ['@daengle/design-system'],
+  reactStrictMode: true,
+  transpilePackages: ['@daengle/design-system', '@daengle/services'],
   env: {
     AWS_REGION: process.env.AWS_REGION,
     AWS_ACCESS_KEY_ID: process.env.AWS_ACCESS_KEY_ID,
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     AWS_BUCKET: process.env.AWS_BUCKET,
   },
-  reactStrictMode: true,
-  transpilePackages: ['@daengle/design-system', '@daengle/services'],
 };
 
 export default nextConfig;
