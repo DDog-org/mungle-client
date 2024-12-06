1bbabc2c80646da448a83caebe1ad9feb5b202c2
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-12-06 17:23:30 +0900

    fix(daengle): 디자인 및 api에 따른 수정

--- apps/daengle/src/components/estimate/option/index.tsx
+++ apps/daengle/src/components/estimate/option/index.tsx
@@ -6,15 +6,6 @@ export function OptionSelector(): JSX.Element {
   const router = useRouter();
   return (
     <div css={wrapper}>
-      <TextButton
-        onClick={() => {
-          alert('해당 요청에 대한 견적을 그만 받으시겠습니까?');
-        }}
-      >
-        <Text typo="body4" color="gray500">
-          견적 그만 받기
-        </Text>
-      </TextButton>
       <TextButton onClick={() => router.push('/temporary-route')}>
         <Text typo="body4" color="gray500">
           내가 보낸 요청
