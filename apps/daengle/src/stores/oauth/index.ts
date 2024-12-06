817c4a881b42634ef6520c7d7176ffe41a5da706
Joohyun Kim <corinthionia@gmail.com>
2024-12-07 02:04:49 +0900

    chore(apps): remove useless codes

--- apps/daengle/src/stores/oauth/index.ts
+++
@@ -1,7 +0,0 @@
-import { create } from 'zustand';
-
-export const useAuthStore = create((set) => ({
-  accessToken: '', // 초기 상태
-  setAccessToken: (token) => set({ accessToken: token }), // 토큰 설정
-  clearAccessToken: () => set({ accessToken: '' }), // 토큰 제거
-}));
