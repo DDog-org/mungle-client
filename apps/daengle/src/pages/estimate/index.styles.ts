de182da88cbdb6b5d3f9c38da4a453452d808bd9
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-12-07 01:48:38 +0900

    feat(daengle): 사용자 일반 & 지정 견적 리스트 구분 페이지 구현 (#170)
    * feat(daengle): 사용자 견적서 리스트 일반, 지정 견적서에 따른 분기 렌더 구현
    
    * fix(design-system): 선택 화살표 크기 및 색상 동적 설정 가능하도록 수정

--- apps/daengle/src/pages/estimate/index.styles.ts
+++ apps/daengle/src/pages/estimate/index.styles.ts
@@ -10,4 +10,63 @@ export const wrapper = css`
 export const headerContainer = css`
   padding: 18px 34px;
   margin-top: 4px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+  cursor: pointer;
+`;
+
+export const line = css`
+  width: 100%;
+  height: 1px;
+  background: ${theme.colors.gray100};
+`;
+
+export const modalItem = (isDesignation: boolean) => css`
+  width: 100%;
+  text-align: center;
+  border: none;
+  color: ${isDesignation ? theme.colors.blue200 : theme.colors.gray400};
+  cursor: pointer;
+
+  :hover {
+    color: ${theme.colors.blue200};
+  }
+`;
+
+export const modalOverlay = css`
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  background: ${theme.colors.grayOpacity300};
+  z-index: 100;
+`;
+
+export const modalContent = css`
+  position: fixed;
+  width: 100%;
+  max-width: ${theme.size.maxWidth};
+  justify-content: center;
+  align-items: center;
+  bottom: 0;
+  background: white;
+  border-radius: 20px 20px 0 0;
+  padding: 21px 18px;
+  z-index: 101;
+  animation: slideUp 0.3s ease-in-out;
+  display: flex;
+  flex-direction: column;
+  gap: 19px;
+
+  @keyframes slideUp {
+    from {
+      transform: translateY(100%);
+    }
+    to {
+      transform: translateY(0);
+    }
+  }
 `;
