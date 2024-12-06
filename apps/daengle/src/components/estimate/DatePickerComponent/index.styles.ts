d93c0b9c7e2b0ea1ad8af98e5fb5ea9fa2951d6c
Yoonjeong Ko <jejukyj@naver.com>
2024-12-07 03:05:25 +0900

    design(daengle): 사용자 병원 견적 요청서 UI 구현 (#177)

---
+++ apps/daengle/src/components/estimate/DatePickerComponent/index.styles.ts
@@ -0,0 +1,17 @@
+import { css } from '@emotion/react';
+
+export const box = css`
+  display: flex;
+  gap: 15px;
+`;
+
+export const dateSelect = css`
+  width: 100%;
+  height: 36px;
+  display: flex;
+  justify-content: center;
+  flex-wrap: wrap;
+  align-items: center;
+  position: relative;
+  cursor: pointer;
+`;
