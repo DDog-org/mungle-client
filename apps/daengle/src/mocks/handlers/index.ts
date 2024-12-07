06d6e2fe34aaabbcc56223e37b2a437e27772f07
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-12-07 21:29:19 +0900

    chore(daengle): mock data 초기 세팅

--- apps/daengle/src/mocks/handlers/index.ts
+++ apps/daengle/src/mocks/handlers/index.ts
@@ -1,56 +1,11 @@
 import { HttpResponse, http } from 'msw';
 
 export const handlers = [
-  http.get('https://api.daengle.com/api/user/estimate/list', () => {
+  http.get('https://api.example.com/api/user', () => {
     return HttpResponse.json({
-      success: true,
-      response: {
-        petInfos: [
-          {
-            petId: 1,
-            name: '강아지A',
-            image: null,
-            groomingEstimates: [
-              {
-                groomingEstimateId: 1,
-                name: '유레카미용사',
-                daengleMeter: 0,
-                proposal: 'GENERAL',
-                image: null,
-                shopName: '유레카 헤어샵',
-                reservedDate: '2024-11-25 11:23:44',
-              },
-            ],
-            careEstimates: [
-              {
-                careEstimateId: 3,
-                name: '다고쳐 댕댕병원',
-                daengleMeter: 0,
-                proposal: 'GENERAL',
-                image: null,
-                reservedDate: '2024-10-25 11:33:22',
-              },
-            ],
-          },
-          {
-            petId: 2,
-            name: '강아지D',
-            image: '반려견 이미지',
-            groomingEstimates: [],
-            careEstimates: [
-              {
-                careEstimateId: 5,
-                name: '다고쳐 댕댕병원',
-                daengleMeter: 0,
-                proposal: 'GENERAL',
-                image: null,
-                reservedDate: '2024-12-25 11:33:22',
-              },
-            ],
-          },
-        ],
-      },
-      error: null,
+      id: 'c7b3d8e0-5e0b-4b0f-8b3a-3b9f4b3d3b3d',
+      firstName: 'John',
+      lastName: 'Maverick',
     });
   }),
 ];
