1318a738a625496e7cfc54dbb63a6a469c07d241
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-12-08 00:02:50 +0900

    refactor(services): 빈 데이터 화면 불필요 props 제거

--- packages/services/src/components/estimate-list/empty-state/index.styles.ts
+++ packages/services/src/components/estimate-list/empty-state/index.styles.ts
@@ -1,20 +1,23 @@
 import { css } from '@emotion/react';
 import { theme } from '@daengle/design-system';
 
-export const wrapper = (hasOptions: boolean) => css`
+export const wrapper = css`
   display: flex;
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  text-align: center;
   gap: 15px;
-  margin-top: ${hasOptions ? '150px' : '100px'};
+
+  margin-top: 243px;
+
+  text-align: center;
 `;
 
 export const emptyButton = css`
   margin-top: 20px;
   border-radius: 29.5px;
-  cursor: pointer;
+
   text-align: center;
   ${theme.typo.body4};
+  cursor: pointer;
 `;
