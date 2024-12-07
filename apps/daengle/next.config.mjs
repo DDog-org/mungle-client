d6057063b331fa9b480a3175d6a4df8ecd802b83
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-12-08 00:51:46 +0900

    chore(daengle): 코드 리뷰 수정 사항 개선

--- apps/daengle/next.config.mjs
+++ apps/daengle/next.config.mjs
@@ -1,6 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
   transpilePackages: ['@daengle/design-system', '@daengle/services'],
   env: {
     AWS_REGION: process.env.AWS_REGION,
@@ -8,8 +7,6 @@ const nextConfig = {
     AWS_SECRET_ACCESS_KEY: process.env.AWS_SECRET_ACCESS_KEY,
     AWS_BUCKET: process.env.AWS_BUCKET,
   },
-  reactStrictMode: true,
-  transpilePackages: ['@daengle/design-system', '@daengle/services'],
 };
 
 export default nextConfig;
