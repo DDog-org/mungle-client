ab04fbeb35ea498020b18b21684d2e6b71930d4a
kyoul10121 <kyoul10121@naver.com>
2024-12-10 02:28:32 +0900

    refactor(daengle): 폴더구조 수정 및 style 파일 통합

--- apps/daengle/src/pages/mypage/user-profile/edit/interfaces/index.ts
+++
@@ -1,9 +0,0 @@
-export interface UserProfileInfoEditForm {
-  image: File | null;
-  nickname: string;
-  isAvailableNickname: boolean;
-}
-export interface UserProfileInfoEditFormType {
-  form: UserProfileInfoEditForm;
-  isAvailableNickname: boolean;
-}
