608b5a984a5993e7b712d191bd26eb49f870c3a5
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-12-09 22:10:36 +0900

    fix(design-system): chip-toggle 컴포넌트 수정

--- packages/core/design-system/src/components/chips/chip-toggle-button/index.tsx
+++ packages/core/design-system/src/components/chips/chip-toggle-button/index.tsx
@@ -9,19 +9,53 @@ interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   size?: Size;
   isSelected?: boolean;
   textColor?: KeyOfColors;
+  itemValue?: string;
+  setSelectedParts?: React.Dispatch<React.SetStateAction<string[]>>;
+  setSelectedTags?: React.Dispatch<React.SetStateAction<string[]>>;
   children: ReactNode;
 }
 
 export function ChipToggleButton({
   size = 'fluid',
   disabled = false,
+  itemValue,
   isSelected = false,
   textColor = 'black',
+  setSelectedParts,
+  setSelectedTags,
   children,
   ...props
 }: Props) {
+  const [isButtonSelected, setIsButtonSelected] = useState<boolean>(isSelected);
+
+  const handleButtonToggle = (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    setIsButtonSelected((prev) => !prev);
+
+    if (setSelectedParts && itemValue) {
+      setSelectedParts((prev) => {
+        if (prev.includes(itemValue)) {
+          return prev.filter((part) => part !== itemValue);
+        }
+        return [...prev, itemValue];
+      });
+    }
+    if (setSelectedTags && itemValue) {
+      setSelectedTags((prev) => {
+        if (prev.includes(itemValue)) {
+          return prev.filter((tag) => tag !== itemValue);
+        }
+        return [...prev, itemValue];
+      });
+    }
+  };
+
   return (
-    <button {...props} css={wrapper({ isSelected, size, disabled, textColor })} disabled={disabled}>
+    <button
+      {...props}
+      value={itemValue}
+      css={wrapper({ isSelected: isButtonSelected, size, disabled, textColor })}
+      onClick={(event) => (disabled ? undefined : handleButtonToggle(event))}
+    >
       {children}
     </button>
   );
