f33cf4a6a8b71df1a69c87b03d77d5f61f96e278
yunil kim <98331998+cabrerainbu@users.noreply.github.com>
2024-12-12 20:06:01 +0900

    design(groomer): 미용사 프로필 수정 화면 퍼블리싱 (#246)
    * [Design/daengle] 반려견 프로필 수정 화면 퍼블리싱 (#134)
    
    * design(daengle): 기본 UI 구현
    
    * design(daengle): wrapper 에 전체 padding 적용
    
    * chore(daengle): 정해진 폴더명으로 수정
    
    * design(daengle): 펫 필터링 기본 UI 구현
    
    * design(daengle): 기본 UI 구현
    
    * design(daengle): 몸무게 문구 추가
    
    * design(daengle): 제목 수정
    
    * feat(daengle): storybook 적용 및 타입 정의
    
    * chore(daengle): 불필요한 코드 삭제
    
    * feat(daengle): 누락된 폼 추가
    
    * chore(daengle): 폰트 색상 변경
    
    * chore(daengle): 불필요한 form 태그 삭제
    
    * refactor(daengle): 코드리뷰 토대로 리팩토링
    
    * feat(daengle): 반려견 등록 페이지 퍼블리싱 (#139)
    
    * design(groomer): 미용사 프로필 화면 구현

---
+++ apps/daengle/src/pages/mypage/pet-profile/edit/index.styles.tsx
@@ -0,0 +1,118 @@
+import { css } from '@emotion/react';
+import { theme } from '@daengle/design-system';
+export const wrapper = css`
+  padding: 18px 18px 104px;
+`;
+export const titleBox = css`
+  margin: 0 0 40px;
+`;
+export const petProfileWrapper = css`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+export const petProfileEditWrapper = css`
+  width: fit-content;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 8px;
+`;
+export const petProfileImageBox = css`
+  width: 70px;
+  height: 70px;
+  border-radius: 50%;
+  overflow: hidden;
+  object-fit: cover;
+  border: 5px solid ${theme.colors.blue200};
+`;
+export const line = css`
+  background-color: ${theme.colors.gray100};
+  width: 100%;
+  height: 7px;
+  margin: 31px 0 32px;
+`;
+export const profileImageWrapper = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin: 32px 0 40px;
+`;
+export const profileImageBox = css`
+  width: 116px;
+  height: 116px;
+  border-radius: 50%;
+  overflow: hidden;
+  object-fit: cover;
+`;
+export const profileEditButtonBox = css`
+  margin-top: 12px;
+  font-size: 14px;
+  color: ${theme.colors.gray400};
+`;
+
+export const inputWrapper = css`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+`;
+export const formBox = css`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+export const weightButtonBox = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 3px;
+  width: 100%;
+`;
+export const toggleButtonBox = css`
+  display: flex;
+  gap: 13px;
+`;
+export const chipToggleButtonBox = css`
+  display: flex;
+  gap: 10px;
+`;
+export const selectChipButtonBox = css`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 6px;
+`;
+export const detailformBox = css`
+  display: flex;
+  flex-direction: column;
+  gap: 12px;
+`;
+export const chipButtonBox = css`
+  display: flex;
+  gap: 7px;
+`;
+export const buttonContainer = css`
+  margin: 32px 0 0;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap: 21px;
+`;
+export const detailInput = css`
+  background-color: ${theme.colors.gray100};
+  height: 136px;
+  border-radius: 10px;
+  padding: 14px;
+  ::placeholder {
+    color: ${theme.colors.gray200};
+  }
+`;
+export const weightWrapper = css`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  text-align: center;
+  gap: 3px;
+`;
