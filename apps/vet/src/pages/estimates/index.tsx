d5b0d265943980652e0be38a336ccfa0f421c9e3
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-12-13 01:22:34 +0900

    design(vet): 병원 견적서 리스트&상세조회 퍼블리싱 (#238)
    * design(vet): 병원 견적서 리스트 퍼블리싱
    
    * feat(design-system): tab 조건부 padding 추가
    
    * design(vet): 병원 견적서 상세조회 퍼블리싱

--- apps/vet/src/pages/estimates/index.tsx
+++ apps/vet/src/pages/estimates/index.tsx
@@ -1,3 +1,59 @@
+import { useState } from 'react';
+import { Card } from '@daengle/services/components';
+import { Layout, Tabs, Text, theme } from '@daengle/design-system';
+import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
+import { GeneralCardList } from '~/components/estimate/general-card-list';
+import { DesignationCardList } from '~/components/estimate/designation-card-list';
+
+const TABS = [
+  {
+    id: 'general',
+    label: '일반 견적서',
+  },
+  {
+    id: 'designation',
+    label: '지정 견적서',
+  },
+];
+
+export default function EstimateList(): JSX.Element {
+  const [, setActivePath] = useState<string>(PATHS.ESTIMATE);
+  const renderContent = (activeTabId: string) => {
+    switch (activeTabId) {
+      case 'general':
+        return <GeneralCardList />;
+      case 'designation':
+        return <DesignationCardList />;
+      default:
+        return <GeneralCardList />;
+    }
+  };
+
+  return (
+    <Layout isAppBarExist={false}>
+      <div css={wrapper}>
+        <header css={headerContainer}>
+          <Text typo="title1">견적</Text>
+        </header>
+        <Tabs tabs={TABS} renderContent={renderContent} isPadding={false} />
+      </div>
+      <GNB />
+    </Layout>
+  );
+}
+
+const wrapper = css`
+  height: 100vh;
+  padding-bottom: 104px;
+
+  background-color: ${theme.colors.background};
+`;
+
+const headerContainer = css`
+  margin-top: 20px;
+  padding: 18px;
+`;
 import { useRouter } from 'next/router';
 import { AppBar, Layout } from '@daengle/design-system';
 import { ROUTES } from '~/constants/commons';
