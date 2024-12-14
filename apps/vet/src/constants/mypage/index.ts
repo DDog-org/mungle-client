80f516a372ac9ddbd66a2f9979009651c243dc7f
yunil kim <98331998+cabrerainbu@users.noreply.github.com>
2024-12-14 22:49:02 +0900

    design(vet): 병원 프로필 관리 페이지 퍼블리싱 (#278)
    * design(vet): 병원 프로필 관리 페이지 퍼블리싱
    
    * design(vet): 영업시간을 위한 datepicker 추가 및 휴무일 지정을 위한 chiptogglebutton 스타일 속성 추가
    
    * design(vet): app-bar 배경색 추가
    
    * chore(vet): 불필요한 코드 삭제

---
+++ apps/vet/src/constants/mypage/index.ts
@@ -0,0 +1,31 @@
+export const VET_DAT_OFF: { value: string; label: string }[] = [
+  {
+    value: 'Monday',
+    label: '월',
+  },
+  {
+    value: 'Tuesday',
+    label: '화',
+  },
+  {
+    value: 'Wednesday',
+    label: '수',
+  },
+  {
+    value: 'Thursday',
+    label: '목',
+  },
+  {
+    value: 'Friday',
+    label: '금',
+  },
+
+  {
+    value: 'Saturday',
+    label: '토',
+  },
+  {
+    value: 'Sunday',
+    label: '일',
+  },
+] as const;
--- apps/vet/src/pages/mypage/index.tsx
+++ apps/vet/src/pages/mypage/index.tsx
@@ -1,16 +1,126 @@
-import { useRouter } from 'next/router';
-import { AppBar, Layout } from '@daengle/design-system';
-import { ROUTES } from '~/constants/commons';
-import { GNB } from '~/components/commons';
+import {
+  AppBar,
+  ChipToggleButton,
+  CTAButton,
+  ImageInput,
+  Input,
+  Layout,
+  Text,
+  theme,
+} from '@daengle/design-system';
+import { css } from '@emotion/react';
+import router from 'next/router';
+import DatePickerComponent from '~/components/mypage/date-picker';
+import { VET_DAT_OFF } from '~/constants/mypage';
 
-export default function Mypage() {
-  const router = useRouter();
-
+export default function vetProfile() {
   return (
-    <Layout>
-      <AppBar onBackClick={router.back} onHomeClick={() => router.push(ROUTES.HOME)} />
+    <Layout isAppBarExist={true}>
+      <AppBar onBackClick={router.back} backgroundColor={theme.colors.white} />
+      <div css={wrapper}>
+        <Text tag="h1" typo="title1" color="black">
+          병원 프로필 관리
+        </Text>
+        <ul css={inputWrapper}>
+          <section css={profileImageWrapper}>
+            <Text tag="h2" typo="subtitle3">
+              이미지 첨부
+            </Text>
+            <ImageInput />
+          </section>
+          <li css={readOnlyTextBox}>
+            <Text tag="h2" typo="subtitle3">
+              병원명
+            </Text>
+            <Text typo="body3" color="gray400">
+              다고쳐병원
+            </Text>
+          </li>
+          <li css={readOnlyTextBox}>
+            <Text tag="h2" typo="subtitle3">
+              영업시간
+            </Text>
+            <DatePickerComponent />
+          </li>
+          <li css={readOnlyTextBox}>
+            <Text tag="h2" typo="subtitle3">
+              휴무일
+            </Text>
 
-      <GNB />
+            <div css={chipButton}>
+              {VET_DAT_OFF.map((item) => {
+                return (
+                  <ChipToggleButton key={item.value} size="circle" isPartnerSelected={true}>
+                    {item.label}
+                  </ChipToggleButton>
+                );
+              })}
+            </div>
+          </li>
+          <li css={readOnlyTextBox}>
+            <Input label="전화번호" />
+          </li>
+          <li css={readOnlyTextBox}>
+            <Text tag="h2" typo="subtitle3">
+              위치
+            </Text>
+            <Text typo="body3" color="gray400">
+              서울특별시 강남구 언주로152길 10
+            </Text>
+          </li>
+          <li css={readOnlyTextBox}>
+            <Text tag="h2" typo="subtitle3">
+              소개
+            </Text>
+            <textarea css={detailInput} placeholder="소개글을 작성해주세요" />
+          </li>
+        </ul>
+        <CTAButton service="partner">수정하기</CTAButton>
+      </div>
     </Layout>
   );
 }
+export const wrapper = css`
+  display: flex;
+  flex-direction: column;
+
+  padding: 18px;
+`;
+export const profileImageWrapper = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  gap: 15px;
+
+  margin: 40px 0 0;
+`;
+export const inputWrapper = css`
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
+
+  margin: 0 0 104px;
+  padding: 0;
+`;
+export const readOnlyTextBox = css`
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+`;
+const detailInput = css`
+  height: 136px;
+  padding: 14px;
+  border-radius: 10px;
+
+  background-color: ${theme.colors.gray100};
+
+  ::placeholder {
+    color: ${theme.colors.gray200};
+    font-size: ${theme.typo.body9};
+  }
+`;
+
+const chipButton = css`
+  display: flex;
+  gap: 7px;
+`;
