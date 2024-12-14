bd578544aa14e3cbf19c4766c86ef44cd1601dad
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-12-14 17:48:08 +0900

    design(daengle): 사용자 예약 리스트 & 상세 조회 퍼블리싱
    * design(daengle): 예약 내역 리스트 & 상세조회 퍼블리싱
    
    * design(design-system): 기본 line tag 배경 삭제

--- apps/daengle/src/pages/reservations/index.tsx
+++ apps/daengle/src/pages/reservations/index.tsx
@@ -28,14 +28,6 @@ export default function Reservations() {
     }
   };
 
-  const { tab = 'groomer' } = router.query;
-
-  const handleTabChange = (activeTabId: string) => {
-    router.push({ pathname: '/reservations', query: { tab: activeTabId } }, undefined, {
-      shallow: true,
-    });
-  };
-
   return (
     <Layout isAppBarExist={false}>
       <section css={wrapper}>
