df7f3b9a61f2cb1ea42d2dcd99b678fa15cf9230
yunil kim <98331998+cabrerainbu@users.noreply.github.com>
2024-12-14 22:49:02 +0900

    design(vet): 병원 프로필 관리 페이지 퍼블리싱 (#278)
    * design(vet): 병원 프로필 관리 페이지 퍼블리싱
    
    * design(vet): 영업시간을 위한 datepicker 추가 및 휴무일 지정을 위한 chiptogglebutton 스타일 속성 추가
    
    * design(vet): app-bar 배경색 추가
    
    * chore(vet): 불필요한 코드 삭제

--- packages/core/design-system/src/components/chips/chip-toggle-button/index.tsx
+++ packages/core/design-system/src/components/chips/chip-toggle-button/index.tsx
@@ -8,6 +8,7 @@ type KeyOfColors = keyof typeof colors;
 interface Props extends ButtonHTMLAttributes<HTMLButtonElement> {
   size?: Size;
   isSelected?: boolean;
+  isPartnerSelected: boolean;
   textColor?: KeyOfColors;
   children: ReactNode;
 }
@@ -16,12 +17,13 @@ export function ChipToggleButton({
   size = 'fluid',
   disabled = false,
   isSelected = false,
+  isPartnerSelected = false,
   textColor = 'black',
   children,
   ...props
 }: Props) {
   return (
-    <button {...props} css={wrapper({ isSelected, size, disabled, textColor })}>
+    <button {...props} css={wrapper({ isSelected, size, disabled, isPartnerSelected, textColor })}>
       {children}
     </button>
   );
