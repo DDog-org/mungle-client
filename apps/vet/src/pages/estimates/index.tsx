54188ecd05bbf4544374ecdbe20e6e032357879a
MOONProd <96318529+MOONProd@users.noreply.github.com>
2024-12-15 02:07:03 +0900

    chore(vet): 불필요한 코드 삭제

--- apps/vet/src/pages/estimates/index.tsx
+++ apps/vet/src/pages/estimates/index.tsx
@@ -65,19 +65,3 @@ const headerContainer = css`
   margin-top: 20px;
   padding: 18px;
 `;
-import { useRouter } from 'next/router';
-import { AppBar, Layout } from '@daengle/design-system';
-import { ROUTES } from '~/constants/commons';
-import { GNB } from '~/components/commons';
-
-export default function Estimates() {
-  const router = useRouter();
-
-  return (
-    <Layout>
-      <AppBar onBackClick={router.back} onHomeClick={() => router.push(ROUTES.HOME)} />
-
-      <GNB />
-    </Layout>
-  );
-}
