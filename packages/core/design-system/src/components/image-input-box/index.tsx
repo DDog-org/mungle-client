8802c13ef4c1a6cd2c77a74f594c0ef871cd6fb0
Joohyun Kim <corinthionia@gmail.com>
2024-12-17 14:06:35 +0900

    fix(design-system): fix build error

--- packages/core/design-system/src/components/image-input-box/index.tsx
+++ packages/core/design-system/src/components/image-input-box/index.tsx
@@ -9,7 +9,6 @@ import {
 } from './index.styles';
 import { TextButton, Text } from '@daengle/design-system';
 import { DefaultImage } from '@daengle/design-system/icons';
-import Image from 'next/image';
 
 interface Props {
   onChange?: (files: File | null) => void;
@@ -49,7 +48,7 @@ export const ImageInputBox = forwardRef(({ onChange, defaultValue }: Props, ref)
         <label css={uploadImageButton}>
           {thumbnailSrc ? (
             <div css={thumbnailImage}>
-              <Image src={thumbnailSrc} alt="썸네일" width={116} height={116} />
+              <img src={thumbnailSrc} alt="썸네일" width={116} height={116} />
             </div>
           ) : (
             <div css={defaultImage}>
