fbb64e73769a5fa70fdf9589fe24d482f8fa37f0
kyoul10121 <kyoul10121@naver.com>
2024-12-18 03:57:57 +0900

    feat(daengle): 미용탭에서 구현 기능 병원탭에 적용

--- apps/daengle/src/components/search/vet-list/index.styles.ts
+++ apps/daengle/src/components/search/vet-list/index.styles.ts
@@ -4,7 +4,18 @@ export const wrapper = css`
   padding-bottom: 110px;
 `;
 
+export const tag = css`
+  display: flex;
+  gap: 6px;
+
+  padding: 14px 18px 24px;
+`;
+
 export const cardBox = css`
+  display: flex;
+  flex-wrap: wrap;
+  gap: 17px;
+
   padding: 0 18px;
 `;
 
--- apps/daengle/src/components/search/vet-list/index.tsx
+++ apps/daengle/src/components/search/vet-list/index.tsx
@@ -1,39 +1,79 @@
-import { useGetUserVetsQuery } from '~/queries/main';
+import { useGetUserShopsQuery } from '~/queries/main';
 import { useAddressFormStore } from '~/stores/main';
 import { Empty } from '@daengle/design-system';
 import { useRouter } from 'next/router';
 import { ROUTES } from '~/constants/commons';
-import { emptyBox, cardBox, wrapper } from './index.styles';
+import { cardBox, emptyBox, wrapper, tag } from './index.styles';
 import { Item } from '../item';
+import { TagButton } from '../tag-button';
+import { useState } from 'react';
 
 export function VetList() {
+  const mockShops = [
+    {
+      shopId: 1,
+      shopImage:
+        'https://daengle.s3.ap-northeast-2.amazonaws.com/groomer/profile-images/XNFJar9K8V4h4syHLspqT',
+      shopName: '멋진 샵 1호점',
+      tag: ['냥냐', '웅야'],
+    },
+    {
+      shopId: 2,
+      shopImage:
+        'https://daengle.s3.ap-northeast-2.amazonaws.com/groomer/profile-images/XNFJar9K8V4h4syHLspqT',
+      shopName: '귀여운 샵 2호점',
+      tag: ['냥냐', '웅야'],
+    },
+    {
+      shopId: 3,
+      shopImage:
+        'https://daengle.s3.ap-northeast-2.amazonaws.com/groomer/profile-images/XNFJar9K8V4h4syHLspqT',
+      shopName: '럭셔리 샵 3호점',
+      tag: ['냥냐', '웅야'],
+    },
+  ];
   const router = useRouter();
-  const { data: vets } = useGetUserVetsQuery();
+  const [isSelected, setIsSelected] = useState<boolean>(false);
+  const { data: shops } = useGetUserShopsQuery();
   const { addressForm } = useAddressFormStore();
-  const filteredVets = vets?.allVets.filter((vet) => vet.vetAddress.includes(addressForm));
+  const filteredShops = shops?.allShops.filter((shop) => shop.shopAddress.includes(addressForm));
 
   const handleCardClick = (id: number) => {
-    router.push(ROUTES.VET_DETAIL(id));
+    router.push(ROUTES.GROOMER_DETAIL(id));
+  };
+
+  const handletoggleButton = () => {
+    setIsSelected((prev) => !prev);
   };
 
   return (
     <div css={wrapper}>
+      <section css={tag}>
+        <TagButton isSelected={isSelected} onClick={handletoggleButton}>
+          #이쁘니
+        </TagButton>
+        <TagButton isSelected={isSelected} onClick={handletoggleButton}>
+          #겸둥이
+        </TagButton>
+      </section>
       <div css={cardBox}>
-        {filteredVets && filteredVets.length > 0 ? (
-          filteredVets?.map((vet) => (
-            <Item
-              key={vet.vetAccountId}
-              image={vet.vetImage}
-              name={vet.vetName}
-              address={vet.vetAddress}
-              onClick={() => handleCardClick(vet.vetAccountId)}
-            />
-          ))
+        {/* {/* {filteredShops && filteredShops.length > 0 ? ( */}
+        {mockShops?.map((shop) => (
+          <Item
+            key={shop.shopId}
+            image={shop.shopImage}
+            name={shop.shopName}
+            tag={shop.tag}
+            onClick={() => handleCardClick(shop.shopId)}
+          />
+        ))}
+
+        {/* ))
         ) : (
           <div css={emptyBox}>
-            <Empty title="해당 주소 주변에 병원이 없어요" />
+            <Empty title="해당 주소 주변에 샵이 없어요" />
           </div>
-        )}
+        )} */}
       </div>
     </div>
   );
