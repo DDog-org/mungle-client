dd12f75a97bb3bba24ab865a5bd6e2847a48c4a4
Yoonjeong Ko <jejukyj@naver.com>
2024-12-18 21:08:24 +0900

    design(daengle): 미용샵(마이샵) 상세 페이지 퍼블리싱 (#397)

---
+++ apps/groomer/src/pages/mypage/shop/[id]/index.tsx
@@ -0,0 +1,147 @@
+import { AppBar, Layout, Text, theme } from '@daengle/design-system';
+import { DetailCall, DetailLocation, DetailTime } from '@daengle/design-system/icons';
+import { ShopDefaultImage } from '@daengle/design-system/images';
+import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
+import ProfileCard from '~/components/mypage/profile-card';
+import { ROUTES } from '~/constants';
+
+export default function GroomerMyShopInfo() {
+  const router = useRouter();
+  return (
+    <Layout isAppBarExist={false}>
+      <AppBar onBackClick={router.back} onHomeClick={() => router.push(ROUTES.HOME)} />
+      <div css={wrapper}>
+        <div css={imageSection}>
+          <ShopDefaultImage />
+          <Text typo="title2" color="white" css={shopName}>
+            꼬꼬마 관리샵
+          </Text>
+        </div>
+        <div css={infoBox}>
+          <section css={topSection}>
+            <section css={infoSection}>
+              <div css={time}>
+                <DetailTime width={20} />
+                <Text typo="body9">매일 00:00 - 23:59</Text>
+              </div>
+              <div css={call}>
+                <DetailCall width={20} />
+                <Text typo="body9">02-000-0000</Text>
+              </div>
+              <div css={address}>
+                <DetailLocation width={20} />
+                <Text typo="body9">서울시 강남구 역삼동</Text>
+              </div>
+            </section>
+            <div css={line} />
+            <section css={infoText}>
+              <Text typo="body1">소개</Text>
+              <Text typo="body10">소개글</Text>
+            </section>
+          </section>
+          <section css={bottomSection}>
+            <div css={textBox}>
+              <Text typo="title2">디자이너</Text>
+              <Text typo="title2">1</Text>
+            </div>
+            <section css={groomerList}>
+              <ProfileCard />
+            </section>
+          </section>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+const wrapper = css`
+  position: relative;
+
+  width: 100%;
+  height: 100%;
+`;
+
+const imageSection = css`
+  position: relative;
+
+  width: 100%;
+  height: 416px;
+`;
+
+const shopName = css`
+  position: absolute;
+  bottom: 180px;
+  left: 24px;
+`;
+
+const infoBox = css`
+  position: absolute;
+  bottom: 0;
+
+  width: 100%;
+  height: 540px;
+  border-radius: 20px 20px 0 0;
+
+  background-color: white;
+`;
+
+const topSection = css`
+  padding: 32px 18px 0;
+
+  border-bottom: 7px solid ${theme.colors.gray100};
+`;
+
+const infoSection = css`
+  display: flex;
+  flex-direction: column;
+  gap: 11px;
+`;
+
+const time = css`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+const call = css`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+const address = css`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  margin-bottom: 24px;
+`;
+
+const line = css`
+  height: 1px;
+
+  background-color: ${theme.colors.gray100};
+`;
+
+const infoText = css`
+  display: flex;
+  flex-direction: column;
+  gap: 11px;
+
+  margin-bottom: 32px;
+  padding-top: 24px;
+`;
+
+const bottomSection = css`
+  padding: 24px 18px 0;
+`;
+
+const textBox = css`
+  display: flex;
+  gap: 4px;
+
+  margin-bottom: 9px;
+`;
+
+const groomerList = css`
+  cursor: pointer;
+`;
