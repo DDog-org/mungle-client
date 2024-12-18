a860d3314f7ec34e6e2cafb5d4daf8ce0f52b6aa
Joohyun Kim <corinthionia@gmail.com>
2024-12-18 14:48:26 +0900

    fix(storybook): update workflow

--- apps/storybook/.storybook/main.ts
+++ apps/storybook/.storybook/main.ts
@@ -1,6 +1,5 @@
 import type { StorybookConfig } from '@storybook/react-vite';
 import { join, dirname } from 'path';
-import { mergeConfig } from 'vite';
 
 function getAbsolutePath(value: string): any {
   return dirname(require.resolve(join(value, 'package.json')));
@@ -15,15 +14,6 @@ const config: StorybookConfig = {
   framework: {
     name: getAbsolutePath('@storybook/react-vite'),
     options: {},
-  },
-  viteFinal: async (config) => {
-    return mergeConfig(config, {
-      build: {
-        rollupOptions: {
-          external: ['@emotion/react/jsx-runtime'],
-        },
-      },
-    });
   },
 };
 export default config;
