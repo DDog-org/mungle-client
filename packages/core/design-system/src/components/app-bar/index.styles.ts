f8ba8c9670b01d4c2b483697fbdb1de7d0a052c8
Yoonjeong Ko <jejukyj@naver.com>
2024-12-18 21:08:24 +0900

    design(daengle): 미용샵(마이샵) 상세 페이지 퍼블리싱 (#397)

--- packages/core/design-system/src/components/app-bar/index.styles.ts
+++ packages/core/design-system/src/components/app-bar/index.styles.ts
@@ -23,11 +23,7 @@ export const contents = css`
 
   width: 100%;
   height: 100%;
-
-  /* AppBar 없는 경우 padding 값 18px로 변경(논의 필요) */
-
-  /* padding: 0 12px 0 8px; */
-  padding: 18px;
+  padding: 0 12px 0 8px;
 
   #title {
     position: absolute;
