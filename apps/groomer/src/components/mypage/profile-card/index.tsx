a87a8ec663b23dd4264833c373d765401f5a5ce8
Yoonjeong Ko <jejukyj@naver.com>
2024-12-19 15:32:50 +0900

    story(groomer): 미용사는 본인의 미용샵 상세 정보를 확인할 수 있다 (#404)
    * design(daengle): 미용샵(마이샵) 상세 페이지 퍼블리싱 (#397)
    
    * feat(groomer): 미용사의 마이샵 상세 페이지 api 연동 (#399)

---
+++ apps/groomer/src/components/mypage/profile-card/index.tsx
@@ -0,0 +1,38 @@
+import { DefaultProfile, Paw } from '@daengle/design-system/icons';
+import { imageStyle, meterTag, profile, profileCard, tag, tags, paw } from './index.styles';
+import { Text } from '@daengle/design-system';
+import Image from 'next/image';
+
+interface Props {
+  groomerName: string;
+  groomerImage: string | null;
+  badges: string[];
+  daengleMeter: number;
+}
+
+export default function ProfileCard({ groomerName, groomerImage, badges, daengleMeter }: Props) {
+  return (
+    <div css={profileCard}>
+      {groomerImage === null ? (
+        <DefaultProfile width={80} height={94} css={imageStyle} />
+      ) : (
+        <Image src={groomerImage} alt="이미지" width={80} height={94} css={imageStyle} />
+      )}
+      <div css={profile}>
+        <Text typo="subtitle3">{groomerName}</Text>
+        <div css={tags}>
+          <Text typo="body2" color="red200" css={meterTag}>
+            <Paw width={9} css={paw} />
+            {daengleMeter}m
+          </Text>
+          <Text typo="body12" color="blue200" css={tag}>
+            #대형견
+          </Text>
+          <Text typo="body12" color="blue200" css={tag}>
+            #노견
+          </Text>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ apps/groomer/src/pages/mypage/shop/[id]/index.tsx
@@ -0,0 +1,190 @@
+import { AppBar, Empty, Layout, Text, theme } from '@daengle/design-system';
+import { DetailCall, DetailLocation, DetailTime } from '@daengle/design-system/icons';
+import { ShopDefaultImage } from '@daengle/design-system/images';
+import { css } from '@emotion/react';
+import { useRouter } from 'next/router';
+import ProfileCard from '~/components/mypage/profile-card';
+import { ROUTES } from '~/constants';
+import { VET_DAT_OFF } from '~/constants/mypage';
+import { GetGroomerMyPageShopInfoRequestParams } from '~/models/mypage';
+import { useGetGroomerMyPageShopInfoQuery } from '~/queries/mypage';
+
+export default function GroomerMyShopInfo() {
+  const router = useRouter();
+  const { id } = router.query;
+  const getShopId = Number(id);
+  const shopParams: GetGroomerMyPageShopInfoRequestParams = { id: getShopId };
+
+  const { data: ShopInfo } = useGetGroomerMyPageShopInfoQuery(shopParams);
+
+  return (
+    <Layout isAppBarExist={false}>
+      <AppBar onBackClick={router.back} onHomeClick={() => router.push(ROUTES.HOME)} />
+      <div css={wrapper}>
+        <div css={imageSection(ShopInfo?.imageUrlList[0])}>
+          {ShopInfo?.imageUrlList[0] ? null : <ShopDefaultImage />}
+          <Text typo="title2" color="white" css={shopName}>
+            {ShopInfo?.shopName}
+          </Text>
+        </div>
+        <div css={infoBox}>
+          <section css={topSection}>
+            <section css={infoSection}>
+              <div css={time}>
+                <DetailTime width={20} />
+                <Text typo="body9">
+                  {ShopInfo?.closedDay?.length
+                    ? `${ShopInfo?.startTime.substring(0, 5)} - ${ShopInfo?.endTime.substring(0, 5)} ${ShopInfo?.closedDay
+                        .map((day) => VET_DAT_OFF.find((item) => item.value === day)?.label || day)
+                        .join(', ')} 휴무`
+                    : `매일 ${ShopInfo?.startTime.substring(0, 5)} - ${ShopInfo?.endTime.substring(0, 5)}`}
+                </Text>
+              </div>
+              <div css={call}>
+                <DetailCall width={20} />
+                <Text typo="body9">{ShopInfo?.shopNumber}</Text>
+              </div>
+              <div css={address}>
+                <DetailLocation width={20} />
+                <Text typo="body9">{ShopInfo?.shopAddress}</Text>
+              </div>
+            </section>
+            <div css={line} />
+            <section css={infoText}>
+              <Text typo="body1">소개</Text>
+              <Text typo="body10">{ShopInfo?.introduction}</Text>
+            </section>
+          </section>
+          <section css={bottomSection}>
+            <div css={textBox}>
+              <Text typo="title2">디자이너</Text>
+              <Text typo="title2">{ShopInfo?.groomers.length}</Text>
+            </div>
+            <section css={groomerList}>
+              {ShopInfo?.groomers.length === 0 ? (
+                <div css={emptyBox}>
+                  <Empty title="해당 샵에 등록된 디자이너가 없습니다." />
+                </div>
+              ) : (
+                ShopInfo?.groomers.map((groomer) => (
+                  <ProfileCard
+                    key={groomer.groomerAccountId}
+                    groomerName={groomer.groomerName}
+                    groomerImage={groomer.groomerImage}
+                    badges={groomer.badges}
+                    daengleMeter={groomer.daengleMeter}
+                  />
+                ))
+              )}
+            </section>
+          </section>
+        </div>
+      </div>
+    </Layout>
+  );
+}
+
+const wrapper = css`
+  position: relative;
+
+  width: 100%;
+  height: 100%;
+`;
+
+const imageSection = (shopImage: string | undefined) => css`
+  position: relative;
+
+  width: 100%;
+  height: 416px;
+
+  background-image: url(${shopImage});
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+const shopName = css`
+  position: absolute;
+  bottom: 180px;
+  left: 24px;
+`;
+
+const infoBox = css`
+  position: absolute;
+  bottom: 0;
+
+  width: 100%;
+  height: 540px;
+  border-radius: 20px 20px 0 0;
+
+  background-color: white;
+`;
+
+const topSection = css`
+  padding: 32px 18px 0;
+
+  border-bottom: 7px solid ${theme.colors.gray100};
+`;
+
+const infoSection = css`
+  display: flex;
+  flex-direction: column;
+  gap: 11px;
+`;
+
+const time = css`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+const call = css`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+`;
+const address = css`
+  display: flex;
+  align-items: center;
+  gap: 8px;
+
+  margin-bottom: 24px;
+`;
+
+const line = css`
+  height: 1px;
+
+  background-color: ${theme.colors.gray100};
+`;
+
+const infoText = css`
+  display: flex;
+  flex-direction: column;
+  gap: 11px;
+
+  margin-bottom: 32px;
+  padding-top: 24px;
+`;
+
+const bottomSection = css`
+  padding: 24px 18px 0;
+`;
+
+const textBox = css`
+  display: flex;
+  gap: 4px;
+
+  margin-bottom: 9px;
+`;
+
+const groomerList = css`
+  cursor: pointer;
+`;
+
+const emptyBox = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 15px;
+
+  margin-top: 80px;
+`;
